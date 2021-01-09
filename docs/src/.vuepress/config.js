995bfcc05ab2f4347a6154294c52d8d6e6f7f00b
bbscout <43731495+bbscout@users.noreply.github.com>
2021-01-09 11:21:11 +0100

    Update config.js

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require('../../package')
 
 module.exports = {
-  base: '/vuepress-test/'
+  base: '/vuepress-test/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
